2268e7e0d24818a544084e8c1876d403ab60dc34
kar7hik <karthikkumar.s@protonmail.com>
2026-06-29 15:55:21 +0530

    changes

--- detector.py
+++ detector.py
@@ -1,13 +1,24 @@
 from pupil_apriltags import Detector
 import cv2
+import config
+
 
 class AprilTagDetector:
     def __init__(self):
         self.detector = Detector(
-            families="tag36h11", nthreads=4, quad_decimate=1.0, quad_sigma=0.0, refine_edges=True,
+            families="tag36h11",
+            nthreads=4,
+            quad_decimate=1.0,
+            quad_sigma=0.0,
+            refine_edges=True,
         )
 
     def detect(self, frame):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        return self.detector.detect(gray)
-    
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+        return self.detector.detect(
+            gray,
+            estimate_tag_pose=True,
+            camera_params=config.CAMERA_PARAMS,
+            tag_size=config.TAG_SIZE,
+        )
