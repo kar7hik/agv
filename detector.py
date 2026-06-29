449a86028971e3628c869ecad9c091c7ee31ef9c
kar7hik <karthikkumar.s@protonmail.com>
2026-06-29 15:09:43 +0530

    correction for detection frame

--- detector.py
+++ detector.py
@@ -1,12 +1,13 @@
 from pupil_apriltags import Detector
-
+import cv2
 
 class AprilTagDetector:
     def __init__(self):
         self.detector = Detector(
-            families="tag36h11", nthreads=4, quad_decimate=1.0, quad_sigma=0.0
+            families="tag36h11", nthreads=4, quad_decimate=1.0, quad_sigma=0.0, refine_edges=True,
         )
 
     def detect(self, frame):
-        return self.detector.detect(frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        return self.detector.detect(gray)
     
