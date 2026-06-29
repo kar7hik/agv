a0c0a1c90a14274de6958d512cd0e92751985620
kar7hik <karthikkumar.s@protonmail.com>
2026-06-29 15:30:53 +0530

    added new visualization file

--- main.py
+++ main.py
@@ -3,46 +3,21 @@ import cv2
 import config
 from camera import Camera
 from detector import AprilTagDetector
-
+from visualization import Visulaization
 
 def main():
     camera = Camera()
     detector = AprilTagDetector()
-
+    visualization = Visulaization()
     camera.start()
 
     try:
         while True:
             frame = camera.get_frame()
             detections = detector.detect(frame)
-
-            for detection in detections:
-                corners = detection.corners.astype(int)
-
-                # Draw tag boundary
-                for i in range(4):
-                    p1 = tuple(corners[i])
-                    p2 = tuple(corners[(i + 1) % 4])
-
-                    cv2.line(frame, p1, p2, (0, 255, 0), 2)
-
-                # Draw center
-                center = tuple(detection.center.astype(int))
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-                # Display Tag ID
-                cv2.putText(
-                    frame,
-                    f"ID: {detection.tag_id}",
-                    (corners[0][0], corners[0][1] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.6,
-                    (255, 255, 0),
-                    2,
-                )
-
-            cv2.imshow("AprilTag Detection", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
-
+            frame = visualization.draw(frame, detections)
+            visualization.show(frame)
+            
             key = cv2.waitKey(1) & 0xFF
 
             if key == ord("q"):
@@ -50,7 +25,7 @@ def main():
 
     finally:
         camera.release()
-        cv2.destroyAllWindows()
+        visualization.close()
 
 
 if __name__ == "__main__":
---
+++ visualization.py
@@ -0,0 +1,34 @@
+import cv2
+
+class Visulaization:
+    def __init__(self):
+        self.window_name = "AprilTag Detection"
+        
+    def draw(self, frame, detections):
+        for detection in detections:
+            corners = detection.corners.astype(int)
+            
+            for i in range(4):
+                p1 = tuple(corners[i])
+                p2 = tuple(corners[(i + 1) % 4])
+                
+                cv2.line(frame, p1, p2, (0, 255, 0), 2)
+                center = tuple(detection.center.astype(int))
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                
+                cv2.putText(
+                    frame,
+                    f"ID: {detection.tag_id}",
+                    (corners[0][0], corners[0][1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 255, 0),
+                    2,
+                )
+        return frame
+    
+    def show(self, frame):
+        cv2.imshow(self.window_name, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        
+    def close(self):
+        
