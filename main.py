cb520aa8cc2856a81bbdc479c966d29db795924b
kar7hik <karthikkumar.s@protonmail.com>
2026-06-29 15:14:54 +0530

    added cv2 visualization

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import config
-import camera
-import detector
+import cv2
+from camera import Camera
+from detector import AprilTagDetector
 
-camera = camera.Camera()
-detector = detector.AprilTagDetector()
-camera.start()
 
-while True:
-    frame = camera.get_frame()
-    detector.detect(frame)
+def main():
+    camera = Camera()
+    detector = AprilTagDetector()
+    
+    camera.start()
+    
+    try:
+        while True:
+            frame = camera.get_frame()
+            detections = detector.detect(frame)
+
+            for detection in detections:
+                cv2.circle(frame, detection.corners[0], 5, (0, 0, 255), 2)
+                cv2.circle(frame, detection.corners[1], 5, (0, 0, 255), 2)
+                cv2.circle(frame, detection.corners[2], 5, (0, 0, 255), 2)
+                cv2.circle(frame, detection.corners[3], 5, (0, 0, 255), 2)
+
+            cv2.imshow("frame", cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            key = cv2.waitKey(1)
+            
+            if key == ord("q"):
+                break
+            
+    finally:
+        camera.release()
+        cv2.destroyAllWindows()
+        
+if 
