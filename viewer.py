be33cfb1023aeceefc5ba45129afe1735bf8dc9a
kar7hik <karthikkumar.s@protonmail.com>
2026-06-29 17:53:59 +0530

    corrections on viewer.py

--- viewer.py
+++ viewer.py
@@ -16,43 +16,71 @@ class Viewer:
 
                 cv2.line(frame, p1, p2, (0, 255, 0), 2)
 
+            # Draw Center Circle
             center = tuple(detection.center.astype(int))
+            x = corners[0][0]
+            y = corners[0][1]
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
+            # Draw TAG ID
             cv2.putText(
                 frame,
                 f"ID: {detection.tag_id}",
-                (corners[0][0], corners[0][1] - 10),
+                (x, y - 10),
                 cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
+                0.6,
                 (0, 255, 0),
                 2,
             )
 
-            if detection.yaw is None:
-                yaw_text = "Yaw: N/A"
+            # Draw Heading
+            if detection.heading is None:
+                heading_text = "Heading: N/A"
             else:
-                yaw_text = f"Yaw: {detection.yaw:.1f} deg"
+                heading_text = f"Heading: {detection.heading:.1f} deg"
 
             cv2.putText(
                 frame,
-                yaw_text,
-                (corners[0][0], corners[0][1] + 20),
+                heading_text,
+                (x, y + 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.55,
+                (255, 0, 0),
+                2,
+            )
+
+            # Draw Lateral Offset
+            if detection.lateral is None:
+                lateral_text = "Yaw: N/A"
+            else:
+                lateral_text = f"Yaw: {detection.lateral:.3f} m"
+
+            cv2.putText(
+                frame,
+                lateral_text,
+                (x, y + 20),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 0.55,
                 (0, 255, 255),
                 2,
             )
 
+            # Draw Distance
+            if detection.distance is None:
+                distance_text = "Distance: N/A"
+            else:
+                distance_text = f"Distance: {detection.distance:.3f} m"
+
             cv2.putText(
                 frame,
-                f"X : {detection.x_offset:.3f} m",
-                (corners[0][0], corners[0][1] + 40),
+                distance_text,
+                (x, y + 30),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 0.55,
                 (255, 0, 255),
                 2,
             )
+
         return frame
 
     def show(self, frame):
