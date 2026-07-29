ca11bec5316c5b8f0833cfc21df7eda81ddaac4d
kar7hik <karthikkumar.s@protonmail.com>
2026-07-29 16:20:18 +0530

    location of text

--- main_v4.py
+++ main_v4.py
@@ -367,7 +367,7 @@ def draw_frame(frame, detections, status_lines=None, highlight_ids=None):
         text_x = max(5, tag_left)
 
         label_text_y = max(18, tag_top - 8)
-        lateral_text_y = tag_bottom + 18
+        lateral_text_y = tag_top + 18
         heading_text_y = lateral_text_y + 16
 
         label = f"ID:{tag_id}"
