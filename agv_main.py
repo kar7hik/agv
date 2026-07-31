a6f44bde111368dd319567ce3069382076a2963b
kar7hik <karthikkumar.s@protonmail.com>
2026-07-31 14:42:16 +0530

    changes

--- agv_main.py
+++ agv_main.py
@@ -402,7 +402,7 @@ def draw_frame(frame, detections, expected_tags, selected_id, status_lines):
         cv2.putText(
             frame,
             label,
-            (text_x, label_y + 10),
+            (text_x, label_y),
             cv2.FONT_HERSHEY_SIMPLEX,
             0.45,
             color,
@@ -437,7 +437,7 @@ def draw_frame(frame, detections, expected_tags, selected_id, status_lines):
         cv2.putText(
             frame,
             yaw_text,
-            (text_x, label_y + 34),
+            (text_x, label_y + 50),
             cv2.FONT_HERSHEY_SIMPLEX,
             0.42,
             COLOR_TEXT,
